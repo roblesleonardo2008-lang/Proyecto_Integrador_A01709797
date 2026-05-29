e3403aabf5de71f5c93c97eeb50beb3abfaed913
roblesleonardo2008-lang <robles.leonardo2008@gmail.com>
2026-05-29 07:02:14 -0600

    Update Proyecto-Avance3.h

--- Proyecto-Avance2.h
+++ Proyecto-Avance2.h
@@ -13,6 +13,9 @@ protected:
 public:
     Vehiculos(string Mo,string Pl,int An,int Ta, int Ef);
     virtual void MostrarInfo() = 0;
+    string getPlacas();
+    string getModelo();
+    int getAnio();
 };
 
 Vehiculos:: Vehiculos(string Mo,string Pl,int An,int Ta, int Ef){
@@ -26,16 +29,27 @@ Vehiculos:: Vehiculos(string Mo,string Pl,int An,int Ta, int Ef){
 void Vehiculos::MostrarInfo(){
     cout<< "Es un "<<Modelo<<" con las placas "<<Placas<<" del año "<<Anio<<endl;
 };
+string Vehiculos::getPlacas(){
+    return Placas;
+}
+string Vehiculos::getModelo(){
+    return Modelo;
+}
+int Vehiculos::getAnio(){
+    return Anio;
+}
 
 class Moto: public Vehiculos{
 private:
     string TipoCadena,Manubrio,Modalidad;
     bool Sidecar;
 public:
+    Moto();
     Moto(string TC,string MA,string MD,bool SD, Vehiculos &m);
     int CalcDistanciaK(int EficieciaL,int Tanque,string TipoCadena);
     void SetSidecar(bool NewSidecar);
     void MostrarInfo () override;
+
 };
 
 Moto::Moto(string TC,string MA,string MD,bool SD,Vehiculos &m )
@@ -76,6 +90,7 @@ private:
     int BolsasAire,Puertas;
     bool AireAcondicionado;
 public:
+    Carro();
     Carro(string CA,int BA,int PU,bool AA,Vehiculos &c);
     int CalcDistanciaK(int EficienciaL, int Tanque, bool AireAcondicionado);
     void BloquearPuertas();
@@ -113,5 +128,75 @@ void Carro:: MostrarInfo(){
     Vehiculos::MostrarInfo();
     cout <<"Tiene una carroceria tipo "<<Carroceria<<" con "<<Puertas<<" puertas y "<<BolsasAire<<" bolsas de aire"<<endl;
 }
+
+class Persona{
+protected:
+    string Nombre,RFC,Domicilio;
+    int ID;
+public:
+    Persona();
+    Persona(string Nombre,string RFC,string Domicilio,int ID);
+    string getNombre();
+    string getRFC();
+    string getDomicilio();
+    int getID();
+};
+Persona::Persona(string NO,string RF,string DO,int ID) {
+    Nombre = NO;
+    RFC = RF;
+    Domicilio = DO;
+}
+string Persona:: getNombre(){
+    return Nombre;
+}
+string Persona :: getRFC(){
+    return RFC;
+}
+string Persona::getDomicilio(){
+    return Domicilio;
+}
+int Persona::getID(){
+    return ID;
+}
+
+class Vender : public Persona{
+private:
+    string placas,modelo,Duenio;
+    Persona Cliente,Vendedor;
+    float Precio;
+public:
+    Vender(float Precio,string Duenio,string placas,Persona cliente, Persona vendedor);
+    void Facturar(Persona Cliente,Persona Vendedor,float Precio,string placas,string Duenio);
+    void TransferirDuenio(Persona Cliente);
+};
+
+Vender::Vender(float PR,string DU,string pL, Persona CL, Persona VE){
+    Precio = PR;
+    Duenio = DU;
+    placas = pL;
+    Cliente = CL;
+    Vendedor = VE;
+};
+void Vender::Facturar(Persona Cliente,Persona Vendedor,float Precio,string placas,string Duenio){
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"-------------------    FACTURA     ------------------"<<endl;
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"Monto pagado : "<<Precio<<endl;
+    cout<<"Placas del vehiculo : "<<placas<<endl;
+    cout<<"Dueño actual : "<<Duenio<<endl;
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"Datos del vendedor"<<endl;
+    cout<<"Nombre : "<<Vendedor.getNombre()<<endl;
+    cout<<"ID : "<<Vendedor.getID()<<endl;
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"Datos del comprador"<<endl;
+    cout<<"Nombre : "<<Cliente.getNombre()<<"RFC : "<<Cliente.getRFC()<<endl;
+    cout<<"Domicilio : "<<Cliente.getDomicilio()<<endl;
+}
+void Vender::TransferirDuenio(Persona Cliente){
+    Duenio = Cliente.getNombre();
+}
+
+
 
 #endif //PROYECTO_AVANCE2_H
